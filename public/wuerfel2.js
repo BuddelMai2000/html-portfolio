dd6566b323292e0073d98cf48884d28c805e8f1d
BuddelMai2000 <elke.doebling@gmx.de>
2024-11-23 20:52:03 +0100

    Update wuerfel2.js

--- public/wuerfel2.js
+++ public/wuerfel2.js
@@ -1,1 +1,15 @@
-document.querySelector("h1").style.color="green";
+function zufall(){
+   return Math.floor(Math.random()*3)
+};
+var bilder=["./images/wuerfel1.png","./images/wuerfel2.png","./images/wuerfel3.png"];
+var wuerfel1=zufall();
+var wuerfel2=zufall();
+var wuerfel3=zufall();
+var wuerfel4=zufall();
+document.querySelector(".w1").setAttribute("src",bilder[wuerfel1]);
+document.querySelector(".w2").setAttribute("src",bilder[wuerfel2]);
+document.querySelector(".w3").setAttribute("src",bilder[wuerfel3]);
+document.querySelector(".w4").setAttribute("src",bilder[wuerfel4]);
+document.querySelector("button").addEventListener("click",function(){
+    location.reload();
+})
